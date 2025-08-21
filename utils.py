08081a57697132c831a312d953e0cdb6263d9de9
Sandeep Pradhan <sandeeppradhan@Sandeeps-MacBook-Air-2.local>
2025-08-22 00:11:24 +0530

    move the LCG function to the library

--- utils.py
+++ utils.py
@@ -11,3 +11,11 @@ def read_matrix(filename):
             matrix.append(row)
     return matrix
 
+def LCG(n_event,a = 1103515245 , c = 12345 , m = 32768 , normalise = True ):
+    event_list = []
+    x_i = 15 
+    for i in range(n_event):
+        y_i = x_i/m
+        event_list.append(y_i)
+        x_i = (a * x_i + c )% m
+    return event_list 
